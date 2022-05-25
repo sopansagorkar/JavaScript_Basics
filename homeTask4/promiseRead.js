cb8e58740052babd7271fab544fbc40bc85e2307
sopansagorkar <sagorkars@gmail.com>
2022-05-25 14:30:59 +0530

    hometask 4 added

---
+++ homeTask4/promiseRead.js
@@ -0,0 +1,13 @@
+var fs = require("fs");
+const fsPromises = require("fs").promises;
+
+const fileName = "./samplefile.txt";
+
+fs.promises
+  .readFile(fileName)
+  .then(function (result) {
+    console.log("" + result);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
---
+++ homeTask4/removeChar.js
@@ -0,0 +1,14 @@
+var arr = ["1357", "2480", "0428", "1234", "2345", "3456", "7531"];
+
+function remove(arr) {
+  var uniqueSet = new Set();
+  for (var i = 0; i < arr.length; i++) {
+    for (var j = 0; j < arr.length; j++) {
+      if (arr[j] == arr[i].split("").sort().join("")) {
+        uniqueSet.add(arr[j]);
+      }
+    }
+  }
+  console.log(uniqueSet);
+}
+remove(arr);
